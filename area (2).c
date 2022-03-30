4fc0a89dd52bda56238f7ae7f4af7baab3dac059
da712 <102531184+da712@users.noreply.github.com>
2022-03-30 18:21:57 +0300

    Add files via upload

---
+++ area (2).c
@@ -0,0 +1,22 @@
+//program to find area of a circle
+#include<stdio.h>
+int main ()
+{
+int r=7;
+int pi=3.142;
+int area=pi*r*r;
+printf("area of a circle %d",area);
+}
+return0;
+
+
+
+
+
+
+
+
+
+
+
+
